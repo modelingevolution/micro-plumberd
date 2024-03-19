dd75e801da13285eeb8bd08d6d028c58121c0aa3
Rafał Maciąg <rafal.maciag@modelingevolution.com>
2024-03-19 16:17:58 +0100

    Throwing exceptions

---
+++ src/MicroPlumberd.Tests/Integration/Services/App.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MicroPlumberd.Tests.Integration.Services;
+
+public class App : IDisposable
+{
+    private IHost host;
+
+    public IHost Configure(Action<IServiceCollection>? configure = null)
+    {
+        host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                configure(services);
+            })
+            .Build();
+
+        return host;
+    }
+
+    public void Dispose()
+    {
+        host?.Dispose();
+    }
+
+
+    public async Task<IServiceProvider> StartAsync()
+    {
+        await host.StartAsync();
+        return host.Services;
+    }
+}
