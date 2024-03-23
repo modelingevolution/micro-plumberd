14a97368545fedd25c168b3317a192815ade2fe1
Rafał Maciąg <rafal.maciag@modelingevolution.com>
2024-03-23 17:55:10 +0100

    cleanup and refactor

--- src/MicroPlumberd.Tests/Unit/ApiTypeRegisterTests.cs
+++ src/MicroPlumberd.Tests/Unit/ApiTypeRegisterTests.cs
@@ -10,58 +10,6 @@ using ModelingEvolution.DirectConnect;
 
 namespace MicroPlumberd.Tests.Unit;
 
-public class ProcessManagerGnerationTests
-{
-    [Fact]
-    public void CommandTypes()
-    {
-        GetCommandTypes<XooProcessManager>().Should().BeEquivalentTo(Expected());
-    }
-
-
-
-    [Fact]
-    public void Types()
-    {
-        GetTypes<XooProcessManager>().Should().BeEquivalentTo(ExpectedTypes.AsEnumerable());
-
-    }
-
-    [Fact]
-    public void StartType()
-    {
-        GetStartType<XooProcessManager>().Should().Be(typeof(FooCreated));
-    }
-
-    IEnumerable<Type> ExpectedTypes 
-    {
-        get
-        {
-            yield return typeof(FooCreated);
-            yield return typeof(BooUpdated);
-            yield return typeof(CommandEnqueued<CreateBoo>);
-        }
-    }
-
-    private Type GetStartType<T>() where T : IProcessManager
-    {
-        return T.StartEvent;
-    }
-    private IEnumerable<Type> GetTypes<T>() where T : ITypeRegister
-    {
-        return T.Types;
-    }
-    private IEnumerable<Type> Expected()
-    {
-        yield return typeof(CommandEnqueued<CreateBoo>);
-        yield return typeof(CommandEnqueued<CreateLoo>);
-    }
-    private IEnumerable<Type> GetCommandTypes<T>() where T : IProcessManager
-    {
-        return T.CommandTypes;
-    }
-}
-
 [TestCategory("Unit")]
 public class ApiTypeRegisterTests
 {
---
+++ src/MicroPlumberd.Tests/Unit/ProcessManagerSourceGenerationTests.cs
@@ -0,0 +1,53 @@
+using FluentAssertions;
+using MicroPlumberd.Tests.AppSrc;
+using MicroPlumberd.Tests.Utils;
+
+namespace MicroPlumberd.Tests.Unit;
+
+[TestCategory("Unit")]
+public class ProcessManagerSourceGenerationTests
+{
+    [Fact]
+    public void CommandTypes()
+    {
+        GetCommandTypes<XooProcessManager>().Should().BeEquivalentTo(ExpectedCommands());
+    }
+
+
+    [Fact]
+    public void Types()
+    {
+        GetTypes<XooProcessManager>().Should().BeEquivalentTo(ExpectedTypes());
+    }
+
+    [Fact]
+    public void StartType()
+    {
+        GetStartType<XooProcessManager>().Should().Be(typeof(FooCreated));
+    }
+
+    IEnumerable<Type> ExpectedTypes()
+    {
+        yield return typeof(FooCreated);
+        yield return typeof(BooUpdated);
+        yield return typeof(CommandEnqueued<CreateBoo>);
+    }
+
+    private Type GetStartType<T>() where T : IProcessManager
+    {
+        return T.StartEvent;
+    }
+    private IEnumerable<Type> GetTypes<T>() where T : ITypeRegister
+    {
+        return T.Types;
+    }
+    private IEnumerable<Type> ExpectedCommands()
+    {
+        yield return typeof(CommandEnqueued<CreateBoo>);
+        yield return typeof(CommandEnqueued<CreateLoo>);
+    }
+    private IEnumerable<Type> GetCommandTypes<T>() where T : IProcessManager
+    {
+        return T.CommandTypes;
+    }
+}
